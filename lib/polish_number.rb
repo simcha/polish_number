2de96c443f5b86e148150d3848bb774c4dc53ea5
Jan Topiński <jan.topinski@in4mates.com>
2016-04-01 07:42:51 +0200

    Code climate fixes

--- lib/polish_number.rb
+++ lib/polish_number.rb
@@ -40,31 +40,69 @@ module PolishNumber
             :one_100 => 'öre', :few_100 => 'öre', :many_100 => 'öre', :gender_100 => :it}
   }
 
-  def self.translate(number, options={})
+  def self.validate(number, options)
     if options[:currency] && !CURRENCIES.has_key?(options[:currency])
-      raise ArgumentError, "unknown :currency option '#{options[:currency].inspect}'. Choose one from: #{CURRENCIES.inspect}"
+      raise ArgumentError, "Unknown :currency option '#{options[:currency].inspect}'." +
+                  " Choose one from: #{CURRENCIES.inspect}"
     end
+
     if options[:cents] && !CENTS.include?(options[:cents])
-      raise ArgumentError, "unknown :cents option '#{options[:cents].inspect}'. Choose one from: #{CENTS.inspect}"
+      raise ArgumentError, "Unknown :cents option '#{options[:cents].inspect}'." +
+                  " Choose one from: #{CENTS.inspect}"
     end
-
-    options[:cents] ||= :auto
-
-    number = number.to_i if options[:cents]==:no
 
     unless (0..999999999).include? number
       raise ArgumentError, 'number should be in 0..999999999 range'
     end
+    options
+  end
 
+  def self.translate(number, options={})
+
+    options = validate(number, options)
+
+    options[:cents] ||= :auto
+    number = number.to_i if options[:cents]==:no
     formatted_number = sprintf('%012.2f', number)
     currency = CURRENCIES[options[:currency] || :NO]
 
+    digits = formatted_number.chars.map { |char| char.to_i }
+    result = process_1_999999999(digits[0..8], options, number, currency)
+
+    process_99_0(result, digits, options, formatted_number[-2..-1], currency)
+
+  end
+
+  def self.add_currency(name, hash)
+    CURRENCIES[name]=hash
+  end
+
+  private
+
+  def self.process_99_0(result, digits, options, formatted_sub_number, currency)
+    if options[:cents] == :words ||
+        (options[:cents] == :auto && formatted_sub_number != '00')
+      digits_cents = digits[-3..-1] if digits
+      number_cents = formatted_sub_number.to_i
+      result << ' i ' unless result.empty?
+      result << process_0_999(digits_cents, number_cents, currency[:gender_100] || :hi) if digits
+      result << ZERO.dup if formatted_sub_number == '00'
+      result.strip!
+      result << ' '
+      result << currency[classify(formatted_sub_number.to_i, digits_cents, true)]
+    elsif options[:cents] == :digits
+      result << ' '
+      result << formatted_sub_number
+      result << '/100'
+    end
+
+    result
+  end
+
+  def self.process_1_999999999(digits, options, number, currency)
     if number == 0
       result = ZERO.dup
     else
-      digits = formatted_number.chars.map { |char| char.to_i }
-      digits_i = digits[0..8]
-
       result = ''
       result << process_0_999(digits[0..2], number, :number)
       result << millions(number.to_i/1000000, digits[0..2])
@@ -79,38 +117,10 @@ module PolishNumber
     end
 
     if options[:currency] && !result.empty?
-      result << ' '
-      result << currency[classify(number.to_i, digits_i)]
+      result << ' ' + currency[classify(number.to_i, digits)]
     end
-
-    if options[:cents] == :words ||
-        (options[:cents] == :auto && formatted_number[-2..-1] != '00')
-      digits_cents = digits[-3..-1] if digits
-      number_cents = formatted_number[-2..-1].to_i
-      result << ' i ' unless result.empty?
-      result << process_0_999(digits_cents, number_cents, currency[:gender_100] || :hi) if digits
-      result << ZERO.dup if formatted_number[-2..-1] == '00'
-      result.strip!
-      result << ' '
-      result << currency[classify(formatted_number[-2..-1].to_i, digits_cents, true)]
-    elsif options[:cents] == :digits
-      result << ' '
-      result << formatted_number[-2..-1]
-      result << '/100'
-    elsif options[:cents] == :digits
-      result << ' '
-      result << formatted_number[-2..-1]
-      result << '/100'
-    end
-
     result
   end
-
-  def self.add_currency(name, hash)
-    CURRENCIES[name]=hash
-  end
-
-  private
 
   def self.process_0_999(digits, number, object)
     result = ''
@@ -120,20 +130,24 @@ module PolishNumber
       result << TEENS[digits[2]]
     else
       result << TENS[digits[1]]
-      if digits[2] == 2 && object == :she
-        result << 'dwie '
-      elsif number == 1 && object == :she
-        result << 'jedna '
-      elsif number == 1 && object == :it
-        result << 'jedno '
-      elsif digits == [0,0,1] && object == :number
-        result << ''
-      else
-        result << UNITIES[digits[2]]
-      end
+      result << process_0_9(digits, number, object)
     end
 
     result
+  end
+
+  def self.process_0_9(digits, number, object)
+    if digits[2] == 2 && object == :she
+      'dwie '
+    elsif number == 1 && object == :she
+      'jedna '
+    elsif number == 1 && object == :it
+      'jedno '
+    elsif digits == [0,0,1] && object == :number
+      ''
+    else
+      UNITIES[digits[2]]
+    end
   end
 
   def self.thousands(number, digits)
